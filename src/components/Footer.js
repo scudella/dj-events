1e579f92035861815d44969c1036e3ae03b3a4af
Eduardo S. Libardi <eslibardi@gmail.com>
2023-02-02 17:04:45 -0300

    Header and Footer components added to the Layout

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+import styles from '@/styles/Footer.module.css';
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <p>Copyright &copy; DJ Events {new Date().getFullYear()}</p>
+      <p>
+        <Link href='/about'>About This Project</Link>
+      </p>
+    </footer>
+  );
+};
+export default Footer;
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/Layout.module.css';
+import Header from './Header';
+import Footer from './Footer';
 
 const Layout = ({ title, keywords, description, children }) => {
   return (
@@ -9,7 +11,9 @@ const Layout = ({ title, keywords, description, children }) => {
         <meta name='description' content={description} />
         <meta name='keywords' content={keywords} />
       </Head>
+      <Header />
       <div className={styles.container}>{children}</div>
+      <Footer />
     </div>
   );
 };
