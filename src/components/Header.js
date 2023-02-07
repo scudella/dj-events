45cdb49ef575a092a1e4b5eb3588a84e7407861e
Eduardo S. Libardi <eslibardi@gmail.com>
2023-02-07 16:20:16 -0300

    Add Event Page

--- src/components/Header.js
+++ src/components/Header.js
@@ -16,6 +16,9 @@ const Header = () => {
           <li>
             <Link href='/events'>Events</Link>
           </li>
+          <li>
+            <Link href='/events/add'>Add Event</Link>
+          </li>
         </ul>
       </nav>
     </header>
