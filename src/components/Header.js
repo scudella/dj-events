8f60357a5320f042a57b5d2308cffbbc43735474
Eduardo S. Libardi <eslibardi@gmail.com>
2023-02-07 15:38:42 -0300

    Add Search component and search input on Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import Search from './Search';
 import styles from '@/styles/Header.module.css';
 
 const Header = () => {
@@ -7,6 +8,9 @@ const Header = () => {
       <div className={styles.logo}>
         <Link href='/'>DJ Events</Link>
       </div>
+
+      <Search />
+
       <nav>
         <ul>
           <li>
