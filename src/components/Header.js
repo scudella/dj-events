7e1914ba9e7da07a36f987de4a39ec1d3f30fb51
Eduardo S. Libardi <eslibardi@gmail.com>
2023-02-12 18:54:48 -0300

    Register and Login form pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
 import Link from 'next/link';
 import Search from './Search';
 import styles from '@/styles/Header.module.css';
@@ -18,6 +19,11 @@ const Header = () => {
           </li>
           <li>
             <Link href='/events/add'>Add Event</Link>
+          </li>
+          <li>
+            <Link href='/account/login' className='btn-secondary btn-icon'>
+              <FaSignInAlt /> Login{' '}
+            </Link>
           </li>
         </ul>
       </nav>
