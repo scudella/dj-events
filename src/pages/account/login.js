6d7bb46e44ae3c3da3f9216bd18aab65e5e9a89e
Eduardo S. Libardi <eslibardi@gmail.com>
2023-02-13 14:22:18 -0300

    Login and get user data at the frontend

--- src/pages/account/login.js
+++ src/pages/account/login.js
@@ -13,6 +13,10 @@ const LoginPage = () => {
 
   const { login, error } = useContext(AuthContext);
 
+  useEffect(() => {
+    error && toast.error(error);
+  });
+
   const handleSubmit = (e) => {
     e.preventDefault();
     login({ email, password });
