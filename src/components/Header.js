593d99a08f58d0942d30b97c8d7354ac31dff64f
Eduardo S. Libardi <eslibardi@gmail.com>
2023-02-13 09:51:16 -0300

    Auth Context

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa';
+import { useContext } from 'react';
 import Link from 'next/link';
 import Search from './Search';
+import AuthContext from '@/context/AuthContext';
 import styles from '@/styles/Header.module.css';
 
 const Header = () => {
+  const { user, logout } = useContext(AuthContext);
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -17,14 +20,31 @@ const Header = () => {
           <li>
             <Link href='/events'>Events</Link>
           </li>
-          <li>
-            <Link href='/events/add'>Add Event</Link>
-          </li>
-          <li>
-            <Link href='/account/login' className='btn-secondary btn-icon'>
-              <FaSignInAlt /> Login{' '}
-            </Link>
-          </li>
+          {user ? (
+            // if logged in
+            <>
+              <li>
+                <Link href='/events/add'>Add Event</Link>
+              </li>
+              <li>
+                <Link href='/account/dashboard'>Dashboard</Link>
+              </li>
+              <li>
+                <button className='btn-secondary btn-icon' onClick={logout}>
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            // if logged out
+            <>
+              <li>
+                <Link href='/account/login' className='btn-secondary btn-icon'>
+                  <FaSignInAlt /> Login{' '}
+                </Link>
+              </li>
+            </>
+          )}
         </ul>
       </nav>
     </header>
--- src/pages/account/register.js
+++ src/pages/account/register.js
@@ -2,6 +2,7 @@ import { FaUser } from 'react-icons/fa';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { useState, useEffect, useContext } from 'react';
+import AuthContext from '@/context/AuthContext';
 import Link from 'next/link';
 import Layout from '@/components/Layout';
 import styles from '@/styles/AuthForm.module.css';
@@ -12,13 +13,15 @@ const RegisterPage = () => {
   const [password, setPassword] = useState('');
   const [passwordConfirm, setPasswordConfirm] = useState('');
 
+  const { register, error } = useContext(AuthContext);
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (password !== passwordConfirm) {
       toast.error('Passwords do not match!');
       return;
     }
-    console.log({ username, email, password });
+    register({ username, email, password });
   };
 
   return (
@@ -65,7 +68,7 @@ const RegisterPage = () => {
               onChange={(e) => setPasswordConfirm(e.target.value)}
             />
           </div>
-          <input type='submit' value='Login' className='btn' />
+          <input type='submit' value='Register' className='btn' />
         </form>
         <p>
           Already have an account? <Link href='/account/login'>Login</Link>
